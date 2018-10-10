30c8fb69f6c8ee0365774e256088e6c8a30cb1b3
xiangli <xiangli1997@gmail.com>
2018-10-10 14:20:05 -0400

    adding manager

---
+++ javascript/manager_script/create_campaign.js
@@ -0,0 +1,16 @@
+
+function generate_map() {
+	var mapOptions = {
+	    center: new google.maps.LatLng(51.5, -0.12),
+	    zoom: 10,
+	    mapTypeId: google.maps.MapTypeId.HYBRID
+	}
+	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
+
+
+
+
+generate_map();
+//
+//<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBu-916DdpKAjTmJNIgngS6HL_kDIKU0aU&callback=myMap"></script>
