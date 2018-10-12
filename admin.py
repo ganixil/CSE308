ceaa01fbb37e23cc0945d6794de79ce69024d5f1
xiangli <xiangli1997@gmail.com>
2018-10-12 17:02:20 -0400

    flask fetching db post in html

--- admin.py
+++ admin.py
@@ -7,7 +7,8 @@ from database import db_session, User, Admin
 #create the admin blueprint
 bp = Blueprint('admin', __name__, url_prefix='/admin')
 
+
 #function to render the admin page and set the admin page url
 @bp.route('/admPage', methods=('GET', 'POST'))
 def admPage():
-    return render_template('admin_html/admin.html')
+	return render_template('admin_html/admin.html',session=db_session,User=User)
