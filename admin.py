17a5d80022caecc9283a09bfd1597ec538b7e2ab
KevinLin101996 <33579906+KevinLin101996@users.noreply.github.com>
2018-10-12 19:06:43 -0400

    new

--- admin.py
+++ admin.py
@@ -11,12 +11,6 @@ bp = Blueprint('admin', __name__, url_prefix='/admin')
 #function to render the admin page and set the admin page url
 @bp.route('/admPage', methods=('GET', 'POST'))
 def admPage():
-<<<<<<< HEAD
 	return render_template('admin_html/admin.html',session=db_session.query(User).order_by(User.id))
-=======
-	return render_template('admin_html/admin.html',session=db_session,User=User)
 
 
-
-	#HEELO
->>>>>>> 4d58368dc95898e477595f9a67d6cdf8face6e81
