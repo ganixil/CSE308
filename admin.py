c91e3e11a0343d4e759ec25c7cfd3f0cfe89c13f
xiangli <xiangli1997@gmail.com>
2018-10-13 15:47:25 -0400

    admin global variable design

--- admin.py
+++ admin.py
@@ -1,6 +1,6 @@
 from flask import (Blueprint, flash, g, redirect, render_template, request, url_for)
 from werkzeug.exceptions import abort
-from database import db_session, User, Admin
+from database import db_session, User, Admin, GlobalVariables
 
 #create the admin blueprint
 bp = Blueprint('admin', __name__, url_prefix='/admin')
@@ -9,6 +9,9 @@ bp = Blueprint('admin', __name__, url_prefix='/admin')
 #function to render the admin page and set the admin page url
 @bp.route('/adminPage', methods=('GET', 'POST'))
 def adminPage():
-	return render_template('admin_html/admin.html',session=db_session.query(User).order_by(User.id))
+	usr_session = db_session.query(User).order_by(User.id)
+	gblvar_session = db_session.query(GlobalVariables).first()
+	print(gblvar_session)
+	return render_template('admin_html/admin.html',usr_session=usr_session, gblvar_session = gblvar_session)
 
 
