aafbf735ef5f70c92c48ce9d4f26c3270baebfbc
xiangli <xiangli1997@gmail.com>
2018-10-13 19:55:35 -0400

    global variable updating

--- admin.py
+++ admin.py
@@ -9,8 +9,23 @@ bp = Blueprint('admin', __name__, url_prefix='/admin')
 #function to render the admin page and set the admin page url
 @bp.route('/adminPage/<u_name>', methods=('GET', 'POST'))
 def adminPage(u_name):
+	if request.method == 'POST':
+		workday = request.form['workday']
+		movspeed = request.form['movspeed']
+		if(workday is None or movspeed is None):
+			usr_session = db_session.query(User).order_by(User.id)
+			gblvar_session = db_session.query(GlobalVariables).first()
+			return render_template('admin_html/admin.html',usr_session=usr_session, gblvar_session = gblvar_session,u_name=u_name)
+		else:
+			gblvar_session = db_session.query(GlobalVariables).first()
+			gblvar_session.workDayLength = int(workday)
+			gblvar_session.averageSpeed = int(movspeed)
+			db_session.commit()
+
+
 	usr_session = db_session.query(User).order_by(User.id)
 	gblvar_session = db_session.query(GlobalVariables).first()
 	return render_template('admin_html/admin.html',usr_session=usr_session, gblvar_session = gblvar_session,u_name=u_name)
+
 
 
--- static/javascript/admin_script/admin.js
+++
@@ -1,7 +0,0 @@
-
-$(function(){
-    // Instantiate UI tabs vertical
-    $( "#tabs-collapsible" ).tabs({
-        collapsible: true
-    });
-});
